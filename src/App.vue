3a821e35d3eca35a2892a235cf4c0b781d151ef2
Floch <gaelle_floch@hotmail.com>
2020-09-07 16:11:17 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <img alt="Vue logo" src="./assets/logo.png"/>
+    <Todo msg="Hello Vue 3.0 + Vite"/>
+</template>
+
+<script>
+    import Todo from './components/Todo.vue'
+
+    export default {
+        name: 'App',
+        components: {
+            Todo
+        }
+    }
+</script>
---
+++ src/components/Todo.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="todoapp">
+    <header class="header">
+      <h1>To Do List</h1>
+      <input type="text">
+    </header>
+  </section>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style src="./todo.css"></style>
