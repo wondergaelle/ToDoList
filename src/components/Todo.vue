c0f69bdb79a4b54d2ef89c0b54d95de4dc69bf37
Floch <gaelle_floch@hotmail.com>
2020-09-07 20:27:38 +0200

    ajout d'une tache todo.vue

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,15 +1,44 @@
 <template>
-  <section class="todoapp">
+  <section class="todoapp container">
     <header class="header">
       <h1>To Do List</h1>
-      <input type="text">
+      <input type="text" class="new-todo" placeholder="Ajouter une tâche" v-model="newTodo" @keyup.enter="addTodo">
     </header>
+
+    <div class="main">
+      <ul class="todo-list">
+        <li class="todo" v-for="todo in todos">
+          <div class="view">
+            <label >{{todo.name}}</label>
+          </div>
+        </li>
+      </ul>
+    </div>
+
   </section>
 
 </template>
 
 <script>
 export default {
+  data(){
+    return{
+      todos:[{
+        name: 'tâche de test',
+        completed:false
+      }],
+      newTodo:''
+    }
+  },
+  methods:{
+    addTodo(){
+      this.todos.push({
+        completed: false,
+        name: this.newTodo
+      })
+      this.newTodo = ''
+    }
+  }
 }
 </script>
 
