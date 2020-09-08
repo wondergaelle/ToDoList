2c908a4414ca90bcb98394a1042e00583d232116
Floch <gaelle_floch@hotmail.com>
2020-09-08 13:53:33 +0200

    edition d'un champ

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -6,24 +6,27 @@
             <input type="text" class="new-todo" placeholder="Ajouter une tâche" v-model="newTodo"
                    @keyup.enter="addTodo">
         </header>
-
+        <!--vue-->
         <div class="main">
             <input type="checkbox" class="toggle-all" v-model="allDone">
             <!--bouton qui peret de cocher toutes les taches => classe associée a une propriété de la vue-->
             <ul class="todo-list">
                 <!--boucle sur le li-->
-                <!--v-bind completed => permet d'indiquer que la tache est completée (change l'apparence)-->
+                <!--v-bind completed => permet d'indiquer que la tache est completée (change l'apparence) et l'edition de celle ci-->
                 <!--filteredTodos méthode dans le state pour récuparation des filtres-->
-                <li class="todo" v-for="todo in filteredTodos" v-bind:class="{completed: todo.completed}">
+                <li class="todo" v-for="todo in filteredTodos"
+                    v-bind:class="{completed: todo.completed, editing: todo === editing}">
                     <div class="view">
                         <!--completed => permet de connaitre l'etat de complétion d'un champ-->
                         <input type="checkbox" v-model="todo.completed" class="toggle">
-                        <label> {{todo.name}} </label>
+                        <!--declencher l edition d'une tache avec dblclick-->
+                        <label @dblclick="editTodo(todo)"> {{todo.name}} </label>
                         <button class="destroy" @click.prevent="deleteTodo(todo)"></button>
                     </div>
+                    <!--edition d'un champ , associée a une nouvelle propriété editing -->
+                    <input type="text" class="edit" v-model="todo.name" @keyup.enter="doneEdit"  @blur="doneEdit" @keyup.esc="cancelEdit" v-focus="todo === editing">
                 </li>
             </ul>
-
         </div>
         <!--v-show permet de faire disparaitre le footer lorsque toutes les tâches sont supprimées ( propriété hasTodo permet de faire réapparaire-->
         <footer class="footer" v-show="hasTodos">
@@ -38,13 +41,16 @@
                 </li>
             </ul>
             <!--le v-show fait apparaitre le button lorsque les tâches sont réalisées-->
-            <button class="clear-completed" v-show="completed" @click.prevent="deleteCompleted"> Supprimer les tâches finies</button>
+            <button class="clear-completed" v-show="completed" @click.prevent="deleteCompleted"> Supprimer les tâches
+                finies
+            </button>
         </footer>
     </section>
 
 </template>
 
 <script>
+
     export default {
 // <!--declaration du state avec les propriétés-->
         data() {
@@ -55,7 +61,10 @@
                 }],
                 newTodo: '',
                 // conservation dans le state du filtre au niveau du template
-                filter: 'all'
+                filter: 'all',
+                // propriété pour éditer une tache qui sauvergardera la tache modifiée
+                editing: null,
+                oldTodo: ''
             }
         },
         methods: {
@@ -75,6 +84,19 @@
             // suppresion des tâches réalisées
             deleteCompleted() {
                 this.todos = this.todos.filter(todo => !todo.completed)
+            },
+            editTodo(todo) {
+                this.editing = todo
+                this.oldTodo = todo.name
+            },
+            // methode permet de quitter apres edition d une tache
+            doneEdit() {
+                this.editing = null
+            },
+            cancelEdit(){
+                this.editing = null
+                this.editing.name= this.oldTodo
+                this.doneEdit()
             }
         },
         // compteur de tâches, avec cette propriété ça peut servir d'avoir getter & setter
@@ -114,6 +136,17 @@
                     return this.todos.filter(todo => todo.completed)
                 }
                 return this.todos
+            }
+        },
+        // creation de directive avec des objets
+        directives: {
+            focus(el, value) {
+                if (value) {
+                    // nextTick : a la prochaine MAJ du dom adoptera ce comportement focus
+                    Vue.nextTick( _ => {
+                        el.focus()
+                    })
+                }
             }
         }
     }
